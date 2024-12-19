561df330eb60f208685b012dacc4697d4103cf77
Pedro Rodriguez <me@pedro.ai>
2024-12-19 12:56:11 -0800

    Update web.py

--- web.py
+++ web.py
@@ -7,7 +7,19 @@ from ipycanvas import Canvas
 st.set_page_config(layout="wide")
 st.title("Photo Framing Tool")
 st.header("Photo Framing Tool")
+st.markdown("""
+This tool helps you determine what frame and margins best fit a photo. To use it, you need:
 
+- Photo Width/Height: This can be in pixels or any other unit. It is used to calculate the aspect ratio.
+- Min Margin: This is the minimum margin (in same unit as frame size below) to use.
+- Frame Width/Height: These should be frame sizes you are considering. It can be any unit, but must match the unit of the min margin
+- You can enter multiple frame sizes
+
+This tool will take all this information, then:
+
+1. For each frame, calculate the margins assuming the smallest margin should be "Min Margin"
+2. For each frame, display the calculated margins and what size the photos should be printed to.
+""")
 
 st.subheader("Enter your photo dimensions to calculate aspect ratio")
 photo_width = st.number_input("Photo Width", value=1.0, min_value=1.0)
